db3cbc047cf8b6f1c625e6a31d5fd0561238f27e
JuniorDemets <demetsjunior25@gmail.com>
2023-12-09 10:25:19 +0100

    Adding the creation of the file users.txt if it's not exist and the modifications of this one if it's exist. It's updated in the UserUpdatedListener.php file

--- JukeBox-webInterface_back-end/app/Listeners/UserUpdatedListener.php
+++ JukeBox-webInterface_back-end/app/Listeners/UserUpdatedListener.php
@@ -5,31 +5,33 @@ namespace App\Listeners;
 use App\Events\UserUpdated;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\File;
 use App\Models\User;
 
 class UserUpdatedListener implements ShouldQueue
 {
     use InteractsWithQueue;
 
-    /**
-     * Gère l'événement UserUpdated.
-     *
-     * @param  UserUpdated  $event
-     * @return void
-     */
     public function handle(UserUpdated $event)
     {
+        // Vérification de l'existence du fichier
+        $filePath = storage_path('../../../db/users.txt');
+
+        if (!File::exists($filePath)) {
+            // Le fichier n'existe pas, on peut le créer
+            File::put($filePath, 'Contenu initial ou vide');
+            Log::info("Le fichier utilisateurs.txt a été créé.");
+        }
+
         $user = $event->user;
 
         // Vérifiez le type d'action (ajout, mise à jour ou suppression)
         if ($event->action === 'created') {
-            // Action lorsqu'un utilisateur est ajouté
             $this->addUser($user);
         } elseif ($event->action === 'updated') {
-            // Action lorsqu'un utilisateur est mis à jour
             $this->updateUser($user);
         } elseif ($event->action === 'deleted') {
-            // Action lorsqu'un utilisateur est supprimé
             $this->deleteUser($user);
         }
     }
@@ -43,6 +45,9 @@ class UserUpdatedListener implements ShouldQueue
             'email' => $user->email,
             'password' => bcrypt($user->password),
         ]);
+
+        // Écriture dans le fichier après l'ajout d'un utilisateur
+        $this->writeToFile("Utilisateur ajouté - ID : {$user->id}, Prénom : {$user->prenom}, Nom : {$user->nom}");
     }
 
     protected function updateUser(User $user)
@@ -58,6 +63,9 @@ class UserUpdatedListener implements ShouldQueue
                 // Ajoutez d'autres champs si nécessaire
             ]);
         }
+
+        // Écriture dans le fichier après la mise à jour d'un utilisateur
+        $this->writeToFile("Utilisateur mis à jour - ID : {$user->id}, Prénom : {$user->prenom}, Nom : {$user->nom}");
     }
 
     protected function deleteUser(User $user)
@@ -68,5 +76,14 @@ class UserUpdatedListener implements ShouldQueue
         if ($existingUser) {
             $existingUser->delete();
         }
+
+        // Écriture dans le fichier après la suppression d'un utilisateur
+        $this->writeToFile("Utilisateur supprimé - ID : {$user->id}, Prénom : {$user->prenom}, Nom : {$user->nom}");
+    }
+
+    protected function writeToFile($content)
+    {
+        $filePath = storage_path('../../../db/users.txt');
+        File::append($filePath, $content . PHP_EOL);
     }
 }
