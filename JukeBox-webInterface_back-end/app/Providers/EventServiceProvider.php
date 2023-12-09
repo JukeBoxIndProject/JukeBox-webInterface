cb0a7cdd23a449627a09124db4b78a9b7bfdcde0
JuniorDemets <demetsjunior25@gmail.com>
2023-12-09 11:02:22 +0100

    Adding the event and listener and controller in the EventServiceProvider. If it's not done, your controller cannot work

--- JukeBox-webInterface_back-end/app/Providers/EventServiceProvider.php
+++ JukeBox-webInterface_back-end/app/Providers/EventServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,6 +17,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        \App\Events\UserUpdated::class => [
+            \App\Listeners\UserUpdatedListener::class,
+        ],
     ];
 
     /**
@@ -25,7 +27,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        parent::boot();
     }
 
     /**
