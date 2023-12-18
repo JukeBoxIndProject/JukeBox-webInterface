67bbb60a22e35b4f53494848b7878a7d009d9de4
JuniorDemets <demetsjunior25@gmail.com>
2023-12-18 11:21:38 +0100

    update CorsMiddleware

--- JukeBox-webInterface_back-end/app/Http/Middleware/CorsMiddleware.php
+++ JukeBox-webInterface_back-end/app/Http/Middleware/CorsMiddleware.php
@@ -9,19 +9,26 @@ class CorsMiddleware
     public function handle($request, Closure $next)
     {
         if ($request->isMethod('OPTIONS')) {
-            return response('', 200)
-                ->withHeaders([
-                    'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
-                    'Access-Control-Allow-Headers' => 'Content-Type, Authorization',
-                    'Content-Type' => 'text/plain',
-                ]);
+            // For preflight requests
+            return response()
+                ->make('')
+                ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+                ->header('Content-Type', 'text/plain');
         }
 
-        return $next($request)
-            ->withHeaders([
-                'Access-Control-Allow-Origin' => '*',
-                'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
-                'Access-Control-Allow-Headers' => 'Content-Type, Authorization',
-            ]);
+        // For actual requests
+        $response = $next($request);
+
+        // Check if the response is a string
+        if (is_string($response)) {
+            // If it's a string, create a response with the string content
+            $response = response($response);
+        }
+
+        return $response
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
 }
